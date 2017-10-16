99acd14f7c2b41e55df8cb891217ff2681ae8d9a
Hendi48 <hendi48@freenet.de>
2017-10-16 22:21:06 +0200

    * Added SystemComponent graph model that can be dragged into AutoDSL as a prime reference.

---
+++ info.scce.cinco.product.autoDSL/src/info/scce/cinco/product/autoDSL/hooks/DropSystemComponentHook.java
@@ -0,0 +1,21 @@
+package info.scce.cinco.product.autoDSL.hooks;
+
+import java.util.List;
+
+import info.scce.cinco.product.autoDSL.autodsl.autodsl.ComponentNode;
+import info.scce.cinco.product.autoDSL.systemcomponent.systemcomponent.SomeNodeComp;
+import de.jabc.cinco.meta.core.ge.style.model.customfeature.CincoPostCreateHook;
+
+public class DropSystemComponentHook extends CincoPostCreateHook<ComponentNode> {
+
+	@Override
+	public void postCreate(ComponentNode droppedNode) {
+		List<SomeNodeComp> l = droppedNode.getSysComp().getSomeNodeComps();
+		if (!l.isEmpty()) {
+			System.out.println("Dropped node contains: " + l.get(0).getLabel());
+		} else {
+			System.out.println("Dropped node contains no elements!");
+		}
+	}
+
+}
