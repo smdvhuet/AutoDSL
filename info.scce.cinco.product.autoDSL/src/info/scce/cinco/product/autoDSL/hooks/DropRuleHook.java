314419374d46ba1b8286de95a10be5b8348164ca
Hendi48 <hendi48@freenet.de>
2017-10-30 15:17:15 +0100

    * Rule filename is shown as node label.

--- info.scce.cinco.product.autoDSL/src/info/scce/cinco/product/autoDSL/hooks/DropRuleHook.java
+++ info.scce.cinco.product.autoDSL/src/info/scce/cinco/product/autoDSL/hooks/DropRuleHook.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 import de.jabc.cinco.meta.runtime.hook.CincoPostCreateHook;
 import info.scce.cinco.product.autoDSL.autodsl.autodsl.ComponentNode;
+import info.scce.cinco.product.autoDSL.rule.rule.Rule;
 import info.scce.cinco.product.autoDSL.rule.rule.Start;
 
 public class DropRuleHook extends CincoPostCreateHook<ComponentNode> {
 
 	@Override
 	public void postCreate(ComponentNode droppedNode) {
-		List<Start> l = droppedNode.getRule().getStarts();
+		Rule rule = droppedNode.getRule();
+		droppedNode.setLabel(rule.eResource().getURI().lastSegment());
+		/*List<Start> l = droppedNode.getRule().getStarts();
 		if (!l.isEmpty()) {
 			System.out.println("Dropped node has outgoing edges: " + l.get(0).getOutgoing());
 		} else {
 			System.out.println("Dropped node contains no Start!");
-		}
+		}*/
 	}
 
 }
