d37ed2adf4e32ebb7da43feedb199f273ea0446f
Brian Jahnke <brianjahnke@gmx.de>
2018-05-31 16:13:59 +0200

    Implemented CSV Debugger in csv files

--- info.scce.cinco.product.autoDSL/cppcode/Debug.cpp
+++ info.scce.cinco.product.autoDSL/cppcode/Debug.cpp
@@ -2,22 +2,46 @@
 
 #ifdef _WIN32
 #include <intrin.h>
-unsigned long long rdtsc() {
-  return __rdtsc();
-}
+unsigned long long rdtsc() { return __rdtsc(); }
 #else
-#include <inttypes.h>
 #include <cstdio>
+#include <inttypes.h>
 unsigned long long rdtsc() {
   unsigned int lo, hi;
-  __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
   return ((uint64_t)hi << 32) | lo;
 }
-#endif 
+#endif
 
-
-size_t gDebug_table_pos;
 size_t gDebug_frame_pos;
-debug_table gDebug_table;
+CSVTable gDebug_table;
 
 ACCPlusPlus::Logger gLogger;
+
+void WriteCSVToFile(const char *filename, CSVTable table) {
+  std::ofstream file(filename, std::ios::out);
+  if (!file.good()) {
+    return;
+  }
+
+  // Write header
+  CSVTable::CSVTableType::iterator it;
+  file << "Frameindex;";
+  for (it = table.columns.begin(); it != table.columns.end(); it++) {
+    file << it->first + ";";
+  }
+
+  file << "\n";
+
+  // Write body
+  for (size_t i = 0; i < table.current_line - 1; i++) {
+    file << table.frames_captured - table.current_line + i << ";";
+    for (it = table.columns.begin(); it != table.columns.end(); it++) {
+      file << it->second.values[i] << ";";
+    }
+
+    file << "\n";
+  }
+
+  file.flush();
+}
--- info.scce.cinco.product.autoDSL/cppcode/Debug.h
+++ info.scce.cinco.product.autoDSL/cppcode/Debug.h
@@ -2,62 +2,80 @@
 #define ACCPLUSPLUS_DEBUG_H_
 
 #ifdef _WIN32
-#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define __FILENAME__                                                           \
+  (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #else
 #include <cstring>
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#endif 
-
-#define DEBUG_MEMORY_HISTORY_LENGTH 256
-#define DEBUG_MEMORY_ENTRIES_COUNT 65536
-
+#define __FILENAME__                                                           \
+  (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
 unsigned long long rdtsc();
 #define CPU_CYCLE rdtsc()
 
-struct debug_location_info {
-  char* file;
-  int line;
-  char* function;
-};
-
-struct debug_event {
-  unsigned long long time;
-  const char* event_name;
-  debug_location_info location;
-  double value;
-};
-
-struct debug_frame {
-  debug_event events[DEBUG_MEMORY_ENTRIES_COUNT];
-};
-
-struct debug_table {
-  debug_frame frames[DEBUG_MEMORY_HISTORY_LENGTH];
-};
-
-
-extern size_t gDebug_table_pos;
-extern size_t gDebug_frame_pos;
-extern debug_table gDebug_table;
-
-#define NEXT_DEBUG_FRAME gDebug_table_pos++; gDebug_frame_pos = 0;
-#define CURRENT_DEBUG_FRAME gDebug_table.frames[gDebug_table_pos & (DEBUG_MEMORY_HISTORY_LENGTH - 1)]
-#define CURRENT_DEBUG_FRAME_ENTRY CURRENT_DEBUG_FRAME.events[(gDebug_frame_pos - 1) & (DEBUG_MEMORY_ENTRIES_COUNT - 1)]
-#define GET_DEBUG_FRAME_ENTRY gDebug_frame_pos++; CURRENT_DEBUG_FRAME_ENTRY
-#define STORE_EVENT(EventName, Value)  GET_DEBUG_FRAME_ENTRY = {CPU_CYCLE, #EventName, {__FILENAME__, __LINE__, __FUNCTION__}, (double)Value};
-
-#define ACC_LOG(EventName, Value) STORE_EVENT(EventName, Value)
-
-#include <iomanip>
 #include "Logger.h"
+#include <iomanip>
 
 extern ACCPlusPlus::Logger gLogger;
 #define ADD_LOGOUTPUT(sink) gLogger.sinks_.push_back(new sink);
 
-#define ACC_LOG2(msg)                                                \
-  gLogger() << std::left << "[" << CPU_CYCLE << "] "                 \
-            << std::setfill(' ') << std::setw(50) << __FILENAME__        \
-            << std::setfill(' ') << std::setw(60) << __FUNCTION__        \
-            << std::setfill(' ') << std::setw(7)  << __LINE__            \
-            << std::setfill(' ') << std::setw(10) <<  msg;
+#define ACC_LOG2(msg)                                                          \
+  gLogger() << std::left << "[" << CPU_CYCLE << "] " << std::setfill(' ')      \
+            << std::setw(50) << __FILENAME__ << std::setfill(' ')              \
+            << std::setw(60) << __FUNCTION__ << std::setfill(' ')              \
+            << std::setw(7) << __LINE__ << std::setfill(' ') << std::setw(10)  \
+            << msg;
+
+#include <cstdio>
+#include <limits>
+#include <map>
+
+#define LOG_HISTORY_LENGTH 4096
+struct CSVTable;
+extern void WriteCSVToFile(const char *filename, CSVTable table);
+
+struct CSVColumn {
+  double values[LOG_HISTORY_LENGTH];
+};
+
+struct CSVTable {
+  typedef std::map<std::string, CSVColumn> CSVTableType;
+  CSVTableType columns;
+  size_t frames_captured = 0;
+  size_t current_line = 0;
+
+  CSVColumn *getColumn(const char *name) {
+    CSVColumn *column = NULL;
+    CSVTableType::iterator lb = columns.lower_bound(name);
+
+    if (lb != columns.end() && !(columns.key_comp()(name, lb->first))) {
+      column = &lb->second;
+    }
+    else {
+      column = new CSVColumn();
+      columns.insert(lb, CSVTableType::value_type(name, *column));
+    }
+
+    return column;
+  }
+
+  void NextFrame() {
+    current_line = frames_captured++;
+    current_line = current_line & LOG_HISTORY_LENGTH - 1;
+
+    ClearRow(current_line);
+  }
+
+  void ClearRow(int index) {
+    CSVTable::CSVTableType::iterator it;
+    for (it = columns.begin(); it != columns.end(); it++) {
+      it->second.values[index] = 0;
+    }
+  }
+};
+
+extern CSVTable gDebug_table;
+#define ACC_LOG(Name, Value)                                                   \
+  gDebug_table.getColumn(Name)->values[gDebug_table.current_line] = Value;
+#define NEXT_FRAME gDebug_table.NextFrame();
+#define WriteCSV(FILENAME) WriteCSVToFile(FILENAME, gDebug_table);
 #endif // ACCPLUSPLUS_DEBUG_H_
